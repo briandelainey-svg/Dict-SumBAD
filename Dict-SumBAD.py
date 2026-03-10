0a08feda657c0e45e49893e8867a727e4b2f060c
briandelainey-svg <brian.delainey@lskysd.ca>
2026-03-10 13:35:24 -0600

    Add files via upload

--- Dict-SumBAD.py
+++ Dict-SumBAD.py
@@ -1,51 +1,6 @@
 #imports
 import pickle
 from pathlib import Path
-#Functions
-def item(items):
-    print('-' * 10)
-    
-    spell = list(items['Spell'].keys())
-    artifact = list(items['Artifact'].keys())
-    weapon = list(items['Weapon'].keys())
-    
-    index = 0
-    print('Weapons:')
-    while index < (len(weapon) / 2):
-        print('    ', items['Weapon'])
-        index += 1
-        
-    index = 0
-    print('Spell:')
-    while index < (len(spell) / 2):
-        print('    ', items['Spell'])
-        index += 1
-        
-    index = 0
-    print('Artifact:')
-    while index < (len(artifact)):
-        print('    ', items['Artifact'])
-        index += 1
-        
-    print('-' * 10)
-        
-def new_item(items):
-    category = input('''Item Category?
-Weapon
-Spell
-Artifact
->>> ''')
-    name = input('''What is your items name?
->>> ''')
-    if category == 'Weapon' or category == 'Spell':
-        damage = input('''How much damage does it do?
->>> ''')
-    price = input('''How much does it cost?
->>> ''')
-    items[category][name] = {}
-    if category == 'Weapon' or category == 'Spell':
-        items[category][name]['Damage'] = damage
-    items[category][name]['Value'] = price
 
 #Dictionary
 items = {
@@ -54,15 +9,66 @@ items = {
     'Artifact' : {}
     }
 
+#Functions
+def item(items):#a list of all items
+    print('-' * 10)#start of list
+    
+    spell = list(items['Spell'].keys())#makes a list of all spell names
+    artifact = list(items['Artifact'].keys())#makes a list of all artifact names
+    weapon = list(items['Weapon'].keys())#makes a list of all weapon names
+    
+    index = 0#prints all weapons
+    print('Weapons:')
+    while index < (len(weapon) / 2):
+        print('    ', items['Weapon'])
+        index += 1
+        
+    index = 0#prints all spells
+    print('Spell:')
+    while index < (len(spell) / 2):
+        print('    ', items['Spell'])
+        index += 1
+        
+    index = 0#prints all artifacts
+    print('Artifact:')
+    while index < (len(artifact)):
+        print('    ', items['Artifact'])
+        index += 1
+        
+    print('-' * 10)#end of list
+        
+def new_item(items):#function for making new items
+    category = input('''Item Category?
+Weapon
+Spell
+Artifact
+>>> ''')#item category
+    name = input('''What is your items name?
+>>> ''')#items name
+    if category == 'Weapon' or category == 'Spell':#checks if item needs damage
+        damage = input('''How much damage does it do?
+>>> ''')#how much damage item does
+    price = input('''How much does it cost?
+>>> ''')#items price
+    
+    items[category][name] = {}#adds item to dictionary
+    if category == 'Weapon' or category == 'Spell':
+        items[category][name]['Damage'] = damage
+    items[category][name]['Value'] = price
+
 #Main
-data = Path('data.pkl')
-if data.exists():
-    with open('data.pkl', 'rb') as pickle_file:
+data = Path('data.pkl')#defines file path
+if data.exists() and data.stat().st_size > 0:#checks if file already exists
+    with open(data, 'rb') as pickle_file:#grabs all items from data.pkl
         items = pickle.load(pickle_file)
+else:
+    with open('data.pkl', 'wb') as pickle_file:#creates data.pkl if it doesnt exist
+        pickle.dump(items, pickle_file, protocol=pickle.HIGHEST_PROTOCOL)
+    
 while True:
-    if not items['Weapon'] and not items['Spell'] and not items['Artifact']:
+    if not items['Weapon'] and not items['Spell'] and not items['Artifact']:#checks if there are any items
         new_item(items)
-    else:
+    else:#if there are items, you can create or remove one
         item(items)
         choice = input('''Are you done?
 1. Yes
@@ -76,6 +82,16 @@ while True:
 >>> ''')
         if choice == '1':
             new_item(items)
+        elif choice == '2':
+            category = input('''Item Category?
+Weapon
+Spell
+Artifact
+>>> ''')
+            choice = input('''What Item do you need to remove?
+>>> ''')
+            del items[category][choice]
+            
 choice = input('''Do you want to save these items?
 1. Yes
 2. No
@@ -86,6 +102,3 @@ if choice == '1':
     if data.exists():
         with open('data.pkl', 'wb') as pickle_file:
             pickle.dump(items, pickle_file)
-    else:
-        with open(file_path, 'wb') as file:
-            pickle.dump(items, file, protocol=pickle.HIGHEST_PROTOCOL)
